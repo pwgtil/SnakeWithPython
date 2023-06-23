fcffbf7fa53a715fec78647c1cf55669d4bb854e
pwgtiluser.email <87475830+pwgtil@users.noreply.github.com>
2023-06-23 16:36:27 +0200

    Snake with Python v0.101 * Position class * first draw of snake and food

--- Snake/__init__.py
+++ Snake/__init__.py
@@ -1,4 +1,5 @@
 import pygame
+from position import Position
 
 pygame.init()
 
@@ -12,23 +13,48 @@ BLUE = (0, 0, 255)
 screen.fill(WHITE)
 pygame.display.update()
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            quit()
+snake = [
+    Position(2, 2),
+    Position(3, 2),
+    Position(4, 2),
+    Position(5, 2),
+    Position(5, 1),
+]
 
 
-def draw_snake_part(x, y):
-    position = (x * CUBE_SIZE,
-                y * CUBE_SIZE,
+def draw_snake_part(pos):
+    position = (pos.x * CUBE_SIZE,
+                pos.y * CUBE_SIZE,
                 CUBE_SIZE,
                 CUBE_SIZE)
     pygame.draw.rect(screen, GREEN, position)
+
+
+def draw_snake(snake):
+    for part in snake:
+        draw_snake_part(part)
+
+
+def draw_food(pos):
+    radius = float(CUBE_SIZE) / 2
+    position = (pos.x * CUBE_SIZE + radius,
+                pos.y * CUBE_SIZE + radius)
+    pygame.draw.circle(screen, BLUE, position, radius)
+
+
+def fill_bg():
+    screen.fill(WHITE)
+
+
+def draw(snake, food):
+    fill_bg()
+    draw_snake(snake)
+    draw_food(food)
     pygame.display.update()
 
 
-def draw_food(x, y):
-    radius = float(CUBE_SIZE) / 2
-    position = (x * CUBE_SIZE + radius,
-                y * CUBE_SIZE + radius)
-    pygame.draw.circle(screen, BLUE, position)
+while True:
+    draw(snake, Position(1, 1))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
